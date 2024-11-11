d08dae8d312e8fd16c9e7f33b13d027fa525c8c3
lcb7 <142332122+lcb7@users.noreply.github.com>
2024-11-11 09:53:03 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,16 +1,34 @@
 let peso=parseFloat(prompt("informe seu peso "));
 let altura=parseFloat(prompt("informe sua altura "));
-if(isNaN(peso)|| isNan(altura)||peso<=0||altura<=0){
+if(isNaN(peso)|| isNaN(altura)||peso<=0||altura<=0){
 document.write("por favor informe valores válidos!!!");
 }else{
-  let imc=(peso/(altura*altura))
-  document.write("o seu imc é igual a: ");
-  if(imc>=18 && imc<=24,9) {
-    document.write(" peso ideal ");}
-    else if(imc>24.9 && imc<=28){
-
+  let imc=(peso/(altura*altura)).toFixed(2)
+  console.log(imc)
+  document.write("o seu imc é igual a: ",imc);
+  document.write("<br>")
+  if(imc < 18) {
+    document.write("Abaixo do peso normal ");}
+    else if(imc>=18 && imc <=24.99){
+      document.write("peso normal");
+    }     
+     else if (imc>=25 && imc<=29.99){
+      document.write("Acima do peso");
     }
-  }
+    else if(imc>=30 && imc<=34.99){
+      document.write("Obesidade grau I");
+    }
+    else if(imc>=35 && imc<=39.99){
+        document.write("Obesidade grau II (severa)")
+    }
+    else if(imc<=40){
+      document.write("Obesidade grau III(Mórbida)")
+    }
+
+      
+     
+    }
+  
 
 
 
